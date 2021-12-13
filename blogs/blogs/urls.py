c3f498e90c04d905e26ac0c6d40035def59e68df
Feras Nawafleh <85874246+feras98nawafleh@users.noreply.github.com>
2021-12-13 15:19:56 +0200

    Update urls.py

--- blogs/blogs/urls.py
+++ blogs/blogs/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/', include('posts.urls'))
+    path('api/v1/posts/', include('posts.urls'))
 ]
